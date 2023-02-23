0a0b136a70eeffc94ede073e201c9e9a1d53fcd1
Douglas Leal <douglas.leal@live.com>
2023-02-23 10:37:30 -0300

    Utilizando DataAnnotations para validar parâmetros de filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,22 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DisplayName("Título")]
         public string? Titulo { get; set; }
+
         public string? Diretor { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DisplayName("Gênero")]
         public string? Genero { get; set; }
+
+        [DisplayName("Duração")]
+        [Range(30, 600, ErrorMessage = "O campo {0} deve ter no mínimo {1} e no máximo {2} minutos.")]
         public int Duracao { get; set; }
     }
 }
